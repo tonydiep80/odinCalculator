9db9bc87858de1d1a6675072fe0e7eb639bbf629
tony <tonydiep80@gmail.com>
2024-01-26 20:20:36 -0600

    AC functionality and cool display

--- js/operations.js
+++ js/operations.js
@@ -6,11 +6,13 @@ window.addEventListener("load", ()=>{
     const display = document.querySelector(".display");
     const calcButtons = document.querySelectorAll(".calcButton");
     const acButton = document.querySelector(".acButton")
+    const operatorButton = document.querySelectorAll(".operatorButton")
     for(let button of calcButtons){
         button.addEventListener('click', ()=>{
             storedValue+=button.textContent
             displayText+=button.textContent
             display.textContent=displayText
+            //If the added text overflows, only show the new content, remove the first number from the display
             if (display.scrollWidth > display.clientWidth) {
                 // Update the content with the new text
                 displayText = displayText.slice(1)
@@ -18,10 +20,33 @@ window.addEventListener("load", ()=>{
             }
         })
     }
+    for(let button of operatorButton){
+        button.addEventListener('click', ()=>{
+
+        })
+    }
+
+    //AC removes the last character
     acButton.addEventListener('click', ()=>{
-        storedValue=""
-        displayText=""
-        display.textContent = "Clear"
+        if(display.innerText==="Clear"){
+            return
+        }
+        //.slice(0,-1) remove the last character (Function of AC)
+        storedValue=storedValue.slice(0,-1)
+        
+        //Just reset the displayText, then shrink it until it fits the screen 
+        displayText = storedValue;
+        display.textContent = displayText
+        while(display.scrollWidth > display.clientWidth) {
+            displayText = displayText.slice(1)
+            display.textContent = displayText
+        }
+
+        //Check if it's empty 
+        if(displayText.length==0){
+            displayText = ""
+            display.textContent="Clear"
+        }
     })
     
 });
